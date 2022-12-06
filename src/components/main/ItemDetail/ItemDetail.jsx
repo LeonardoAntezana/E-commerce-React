19bec644373375b00a65e55fd3b2c82560e50ae8
Leonardo Antezana <leonardoantezana59@gmail.com>
2022-12-06 15:13:47 -0300

    agregando estilos al itemDetail

--- src/components/main/ItemDetail/ItemDetail.jsx
+++ src/components/main/ItemDetail/ItemDetail.jsx
@@ -1,4 +1,5 @@
 import {React, useState} from 'react'
+import './itemDetail.css'
 import ItemCount from '../products/ItemCount'
 import {Link} from 'react-router-dom'
 
@@ -10,12 +11,16 @@ function ItemDetail( {item} ) {
 
   const {title, description, price, pictureUrl} = item
   return (
-    <div className='d-flex flex-column align-items-center gap-2'>
-        <img className='w-50' src={pictureUrl} alt={description} />
-        <h2>{title}</h2>
-        <p>{price}</p>
-        <p>{description}</p>
-      {quantity === undefined ? <ItemCount stock={2} initial={1} onAdd={onAdd}/> : <Link to={'/cart'}><button>Finalizar compra</button></Link>}
+    <div className='bg-dark py-3 d-flex justify-content-center'>
+      <div className='item-detail d-flex flex-column align-items-center gap-4'>
+        <div className='text-light d-flex flex-column align-items-center'>
+          <img className='img__detail' src={pictureUrl} alt={description} />
+          <h2>{title}</h2>
+          <p>{price}</p>
+          <p>{description}</p>
+        </div>
+        {quantity === undefined ? <ItemCount stock={2} initial={1} onAdd={onAdd}/> : <Link to={'/cart'}><button>Finalizar compra</button></Link>}
+      </div>
     </div>
   )
 }
--- src/components/main/products/ItemCount.jsx
+++ src/components/main/products/ItemCount.jsx
@@ -6,11 +6,11 @@ import './ItemCount.css'
 function ItemCount( {stock, initial, onAdd} ) {
   const [cont, setCont] = useState(initial)
   return (
-    <div>
-        <div className='d-flex'>
-            <button onClick={() => {cont > 1 ?setCont(cont - 1) : setCont(1)}} className='d-flex align-items-center'><FaMinus/></button>
-            <span id='contador' className='text-info'>{cont}</span>
-            <button onClick={() => {cont < stock ? setCont(cont + 1): setCont(stock)}} className='d-flex align-items-center'><FaPlus/></button>
+    <div className='item-count d-flex flex-column align-items-center gap-2'>
+        <div className='w-100 d-flex justify-content-around'>
+          <button onClick={() => {cont > 1 ?setCont(cont - 1) : setCont(1)}} className='d-flex align-items-center'><FaMinus/></button>
+          <span id='contador' className='text-light'>{cont}</span>
+          <button onClick={() => {cont < stock ? setCont(cont + 1): setCont(stock)}} className='d-flex align-items-center'><FaPlus/></button>
         </div>
         <button onClick={() => onAdd(cont)} className={`botonAdd btn ${stock === 0 && 'disabled'}`} >Add</button>
         <p className='text-danger fst-italic'>El stock disponible es: {stock}</p>
