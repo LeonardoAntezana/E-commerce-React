f7f242960d024c2dbf854b4d5ab15f7906840d50
¨Leonardo <leonardoantezana59@gmail.com>
2022-12-27 10:01:11 -0300

    ultimando detalles

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from './components/footer/Footer';
 
 function App() {
   return (
-  <BrowserRouter basename='/'>
+  <BrowserRouter>
     <div className="App">
         <CartContextProvider>
       <NavBar/>
